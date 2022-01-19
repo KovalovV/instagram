e1b9cd0a856ad01eea6dfee5f948d7c0b3565a17
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-01-19 17:41:20 +0200

    test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
+// import mainPageIcon from "./assets/icons/mainPageIcon.svg";
+
+function App() {
+  return (
+    <div>
+      <header>Instagram</header>
+    </div>
+  );
+}
+
+export default App;
