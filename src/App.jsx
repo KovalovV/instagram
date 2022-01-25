1014df20e4aa44d3b780e3936d933aa58c37dcef
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-01-25 13:02:02 +0200

    app root file with routes, store & theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 /* eslint-disable jsx-a11y/alt-text */
-// import mainPageIcon from "./assets/icons/mainPageIcon.svg";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "store";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { getTheme } from "config/theme";
+
+import SignIn from "pages/sign-in";
+import SignUp from "pages/sign-up";
+import ForgotPassword from "pages/forgot-password";
+import Home from "pages/home";
+
+import GlobalStyles from "styles/globalStyles";
 
 function App() {
   return (
-    <Router>
-      <nav>Navigation panell</nav>
-      <div>
-        <header>Instagram</header>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <ThemeProvider theme={getTheme()}>
+        <Router>
+          <GlobalStyles />
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
