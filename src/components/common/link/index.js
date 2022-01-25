9bdeeb0811427ba5219dcc13a79bd7b974c7e3bd
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-01-25 13:00:38 +0200

    link common component

---
+++ src/components/common/link/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledLink = styled.div`
+  a {
+    display: inline-block;
+    font-size: 14px;
+    font-weight: 600;
+    color: #0095f6;
+    text-decoration: none;
+  }
+`;
