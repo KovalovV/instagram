e0551d45ed222f8244476792f76cab331a6f4a1b
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-01-25 12:59:36 +0200

    user actions

---
+++ src/store/actions/user.js
@@ -0,0 +1,13 @@
+export const setCurrentUser = (userData) => ({
+  type: "SET_CURRENT_USER",
+  payload: userData,
+});
+
+//   export const setIsUserLoading: (state, { payload }) => {
+//     isUserLoading = payload;
+//   };
+
+//   export const logOutUser: (state) => {
+//     currentUser = {};
+//     authed = false;
+//   };
