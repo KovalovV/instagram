fa24763544350a06da8ff4465be72bf47725af0d
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-01-25 12:50:04 +0200

    config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,29 @@ module.exports = {
     es2021: true,
   },
   extends: ["airbnb", "plugin:react-hooks/recommended", "prettier"],
+  parser: "babel-eslint",
   parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
+      experimentalObjectRestSpread: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+  },
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+      },
+    },
   },
   plugins: ["react", "react-hooks"],
   rules: {
+    "jsx-a11y/label-has-for": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+    "react/function-component-definition": 1,
+    "react/prop-types": 1,
     "import/no-extraneous-dependencies": 0,
     "import/prefer-default-export": 0,
     "react/react-in-jsx-scope": 0,
@@ -26,4 +40,13 @@ module.exports = {
       { props: true, ignorePropertyModificationsFor: ["state"] },
     ],
   },
+  overrides: [
+    {
+      files: ["*.jsx"],
+      rules: {
+        "react/function-component-definition": "off",
+        "react/prop-types": "off",
+      },
+    },
+  ],
 };
