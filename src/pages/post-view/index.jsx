e42676d0bcf8029884d6529fd9d1911770a5b048
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-02-02 23:33:40 +0200

    post-view page

---
+++ src/pages/post-view/index.jsx
@@ -0,0 +1,157 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useNavigate, Link } from "react-router-dom";
+
+import { formatDate } from "utils/date";
+
+import {
+  setSelectedPostThunk,
+  setSelectedPostCommentsThunk,
+} from "store/thunks/post";
+import { setCurrentUserThunk } from "store/thunks/user";
+
+import PostActions from "components/post-view/post-actions";
+import AddComment from "components/post-view/comment";
+
+import { Icon } from "components/common/icons";
+import Date from "components/common/date";
+
+import {
+  ModalWrapper,
+  PostContainer,
+  Image,
+  MobileImage,
+  Details,
+  HeaderDetails,
+  Comments,
+  ActionContainer,
+  DescriptionHeader,
+  LikeInfo,
+} from "./styles";
+
+const PostView = () => {
+  const params = useParams();
+  const { postId } = params;
+
+  const navigate = useNavigate();
+  const modalWrap = useRef(null);
+
+  const { login, avatar } = useSelector(
+    (state) => state.selectedUser.selectedUserProfile
+  );
+  const {
+    image,
+    description,
+    likes,
+    timestamp: postTimestamp,
+  } = useSelector((state) => state.post.selectedPost);
+  const comments = useSelector((state) => state.post.comments);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setCurrentUserThunk());
+    dispatch(setSelectedPostThunk(postId));
+    dispatch(setSelectedPostCommentsThunk(postId));
+  }, [dispatch, postId]);
+
+  const onClickClose = () => {
+    modalWrap.current.style.display = "none";
+    navigate(`/${login}`);
+    return false;
+  };
+
+  const onKeyDownClose = (key) => {
+    if (key.keyCode === 27) {
+      modalWrap.current.style.display = "none";
+      navigate(`/${login}`);
+    }
+    return false;
+  };
+
+  return (
+    <ModalWrapper ref={modalWrap} onKeyDown={onKeyDownClose} tabIndex="0">
+      <Icon className="close" icon="closeModalIcon" onClick={onClickClose} />
+      <PostContainer>
+        <Image>
+          <div className="image-container">
+            <img src={image} alt="Post" />
+          </div>
+        </Image>
+        <Details>
+          <HeaderDetails>
+            <Link to={`/${login}`}>
+              <img src={avatar} alt="Avatar" />
+            </Link>
+            <h1>
+              <Link to={`/${login}`}>{login}</Link>
+            </h1>
+          </HeaderDetails>
+          <MobileImage>
+            <div className="image-container">
+              <img src={image} alt="Post" />
+            </div>
+          </MobileImage>
+          <Comments>
+            <div className="description">
+              <DescriptionHeader>
+                <Link to={`/${login}`}>
+                  <img src={avatar} alt="Avatar" />
+                </Link>
+                <div>
+                  <span>
+                    <span className="login">
+                      <Link to={`/${login}`}>{login}</Link>
+                    </span>{" "}
+                    {description}
+                  </span>
+                  <Date>
+                    {postTimestamp && formatDate(postTimestamp.seconds * 1000)}
+                  </Date>
+                </div>
+              </DescriptionHeader>
+              {comments &&
+                comments.map(
+                  ({
+                    id,
+                    content,
+                    timestamp: commentTimestamp,
+                    user: { avatar: commentAvatar, login: commentLogin },
+                  }) => (
+                    <DescriptionHeader key={id}>
+                      <Link to={`/${commentLogin}`}>
+                        <img src={commentAvatar} alt="Avatar" />
+                      </Link>
+                      <div>
+                        <span>
+                          <span className="login">
+                            <Link to={`/${commentLogin}`}>{commentLogin}</Link>
+                          </span>{" "}
+                          {content}
+                        </span>
+                        <Date>
+                          {commentTimestamp &&
+                            formatDate(commentTimestamp.seconds * 1000)}
+                        </Date>
+                      </div>
+                    </DescriptionHeader>
+                  )
+                )}
+            </div>
+          </Comments>
+          <ActionContainer>
+            <PostActions />
+            <LikeInfo>{likes && likes.length} likes</LikeInfo>
+            <Date uppercase marginLeft>
+              {postTimestamp && formatDate(postTimestamp.seconds * 1000)}
+            </Date>
+            <AddComment postId={postId} />
+          </ActionContainer>
+        </Details>
+      </PostContainer>
+    </ModalWrapper>
+  );
+};
+
+export default PostView;
---
+++ src/pages/post-view/styles.js
@@ -0,0 +1,206 @@
+import styled, { css } from "styled-components";
+import { flex } from "utils/flex";
+
+export const ModalWrapper = styled.div`
+  ${flex.center}
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.85);
+
+  z-index: 20;
+
+  .close {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+  }
+`;
+
+export const PostContainer = styled.div(
+  ({ theme: { up } }) => css`
+    display: flex;
+    background-color: #fff;
+    color: #262626;
+    border-radius: 10px;
+
+    min-height: 480px;
+    max-height: 650px;
+    width: 100%;
+    max-width: 1000px;
+    margin: 24px 64px;
+
+    ${up(735)} {
+      border-top: none;
+    }
+  `
+);
+
+export const Image = styled.div(
+  ({ theme: { up } }) => css`
+    display: none;
+    ${flex.center}
+    width: 0%;
+    min-height: 280px;
+    max-height: 650px;
+    background-color: #000;
+
+    .image-container {
+      ${flex.center}
+      width: 100%;
+    }
+
+    img {
+      display: none;
+      width: 100%;
+      min-height: auto;
+      max-height: 650px;
+    }
+
+    ${up(735)} {
+      display: flex;
+      width: 100%;
+
+      img {
+        display: block;
+      }
+    }
+  `
+);
+
+export const MobileImage = styled.div(
+  ({ theme: { down } }) => css`
+    display: none;
+    /* ${flex.center} */
+    width: 100%;
+    min-height: 280px;
+    max-height: 650px;
+    background-color: #000;
+
+    .image-container {
+      ${flex.center}
+      width: 100%;
+    }
+
+    img {
+      width: 100%;
+      min-height: auto;
+      max-height: 650px;
+    }
+
+    ${down(735)} {
+      display: flex;
+      min-height: 240px;
+
+      img {
+        width: auto;
+        height: 100%;
+      }
+    }
+  `
+);
+
+export const Details = styled.div(
+  ({ theme: { up } }) => css`
+    position: relative;
+    width: 100%;
+    min-width: 355px;
+
+    ${up(735)} {
+      max-width: 400px;
+    }
+  `
+);
+
+export const HeaderDetails = styled.div`
+  ${flex.alignCenter}
+  width: 100%;
+  height: 60px;
+  padding: 15px;
+
+  img {
+    width: 32px;
+    height: 32px;
+    margin-right: 10px;
+  }
+
+  h1 {
+    color: #262626;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+  }
+`;
+
+export const Comments = styled.div(
+  ({ theme: { up } }) => css`
+    display: none;
+    overflow-y: scroll;
+    scrollbar-width: none;
+    padding: 15px;
+    max-height: calc(100% - 225px);
+    max-width: 100%;
+    border-top: 1px solid #dbdbdb;
+
+    .description {
+      width: 100%;
+      height: auto;
+    }
+
+    ${up(735)} {
+      display: block;
+    }
+  `
+);
+
+export const DescriptionHeader = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+
+  img {
+    width: 32px;
+    height: 32px;
+    margin-right: 10px;
+  }
+
+  span {
+    line-height: 20px;
+  }
+
+  .login {
+    color: #262626;
+    font-weight: 600;
+    font-size: 16px;
+
+    margin-top: -5px;
+    margin-right: 5px;
+    display: inline-flex;
+  }
+
+  .publacation-date {
+    color: #8e8e8e;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 16px;
+    margin: 15px 0px;
+  }
+`;
+
+export const ActionContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  border-top: 1px solid #dbdbdb;
+  border-bottom-right-radius: 10px;
+  background-color: #fff;
+`;
+
+export const LikeInfo = styled.div`
+  padding: 0px 16px;
+  color: #262626;
+  font-weight: 600;
+`;
