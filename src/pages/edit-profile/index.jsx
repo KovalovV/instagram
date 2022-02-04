573369e8be6805e4c4f91f8378b4e7361cea0708
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-02-04 18:27:39 +0200

    edit-profile page component

--- src/pages/edit-profile/index.jsx
+++ src/pages/edit-profile/index.jsx
@@ -1,3 +1,180 @@
-const EditProfile = () => <div>Edit Pfile</div>;
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
+import { api } from "api";
+
+import { setUpdatedUserThunk } from "store/thunks/user";
+
+import { toast } from "react-toastify";
+
+import Input from "components/common/input";
+import Button from "components/common/button";
+
+import {
+  Flex,
+  MainContainer,
+  EditForm,
+  Image,
+  PropLabel,
+  InputDesc,
+} from "./styles";
+
+const EditProfile = () => {
+  const dispatch = useDispatch();
+
+  const {
+    id: currentUserId,
+    name: currentName,
+    login: currentLogin,
+    avatar: currentAvatar,
+    website: currentWebsite,
+    bio: currentBio,
+    email: currentEmail,
+  } = useSelector((state) => state.user.currentUser);
+
+  const [newAvatar, setNewAvatar] = useState(currentAvatar);
+  const [userProfileData, setUserProfileData] = useState({
+    id: currentUserId,
+    name: "",
+    login: "",
+    avatar: "",
+    website: "",
+    bio: "",
+    email: "",
+  });
+
+  const inputFields = [
+    {
+      label: "Name",
+      inputValue: userProfileData.name,
+      inputDescription:
+        "Help people discover your account by using the name you're known by: either your full name, nickname, or business name.",
+    },
+    {
+      label: "Username",
+      inputValue: userProfileData.login,
+      inputDescription: `In most cases, you'll be able to change your username back to ${userProfileData.login} for another 14 days.`,
+    },
+    {
+      label: "Website",
+      inputValue: userProfileData.website,
+      inputDescription: "",
+    },
+    {
+      label: "Bio",
+      inputValue: userProfileData.bio,
+      inputDescription: "",
+    },
+    {
+      label: "Email",
+      inputValue: userProfileData.email,
+      inputDescription:
+        "Provide your personal information, even if the account is used for a business, a pet or something else. This won't be a part of your public profile.",
+    },
+  ];
+
+  useEffect(() => {
+    setUserProfileData({
+      id: currentUserId,
+      name: currentName,
+      login: currentLogin,
+      avatar: currentAvatar,
+      website: currentWebsite,
+      bio: currentBio,
+      email: currentEmail,
+    });
+    setNewAvatar(currentAvatar);
+  }, [
+    currentUserId,
+    currentBio,
+    currentEmail,
+    currentName,
+    currentLogin,
+    currentWebsite,
+    currentAvatar,
+  ]);
+
+  const handleImage = async (e) => {
+    setNewAvatar(e.target.files[0]);
+  };
+
+  const onChangeEdit = (e) => {
+    setUserProfileData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const getImageUrl = async () => {
+    const imageUrls = await api.store.storeImage(currentUserId, newAvatar);
+    setUserProfileData((prevState) => ({
+      ...prevState,
+      avatar: imageUrls,
+    }));
+    return imageUrls;
+  };
+
+  const onSubmitEdit = async (e) => {
+    e.preventDefault();
+    try {
+      const imageURL =
+        newAvatar === userProfileData.avatar
+          ? userProfileData.avatar
+          : await getImageUrl();
+
+      await api.user.updateProfileInfo({
+        ...userProfileData,
+        avatar: imageURL,
+      });
+      dispatch(setUpdatedUserThunk(userProfileData.id));
+    } catch (error) {
+      toast.error("Error");
+    }
+  };
+
+  return (
+    <MainContainer>
+      <EditForm onSubmit={onSubmitEdit}>
+        <Image>
+          <img src={userProfileData.avatar} alt="Avatar" />
+          <div className="change-avatar">
+            <h1>{currentLogin}</h1>
+            <label htmlFor="avatar">Change Profile Photo</label>
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              max="1"
+              accept=".jpg,.png,.jpeg"
+              onChange={handleImage}
+            />
+          </div>
+        </Image>
+        {inputFields.map(({ label, inputValue, inputDescription }) => (
+          <Flex key={`${label}`}>
+            <PropLabel htmlFor={`${label}`}>{label}</PropLabel>
+            <InputDesc>
+              <Input
+                type="textEditProfile"
+                border="dark"
+                bgColor="transparent"
+                id={label === "Username" ? "login" : label.toLowerCase()}
+                placeholder={label}
+                value={inputValue}
+                disabled={label.toLowerCase() === "email"}
+                onChange={onChangeEdit}
+              />
+              <p>{inputDescription}</p>
+            </InputDesc>
+          </Flex>
+        ))}
+        <Button size="default" color="white" bgColor="blue" type="submitEdit">
+          Submit
+        </Button>
+      </EditForm>
+    </MainContainer>
+  );
+};
 export default EditProfile;
--- src/pages/edit-profile/styles.js
+++ src/pages/edit-profile/styles.js
@@ -1,15 +1,113 @@
 import styled, { css } from "styled-components";
+import { flex } from "utils/flex";
 
 export const Card = styled.div(
   ({ theme: { up, breakpoints } }) => css`
     width: 100%;
+    background-color: #fff;
 
     ${up(breakpoints.sm)} {
-      padding: 20px 0px;
-      background-color: #fff;
       border: 1px solid #dbdbdb;
     }
   `
 );
 
-export const MainContainer = styled(Card)``;
+export const Flex = styled.div(
+  ({ theme: { down } }) => css`
+    display: flex;
+    width: 100%;
+    max-width: 555px;
+
+    ${down(735)} {
+      flex-direction: column;
+    }
+  `
+);
+
+export const MainContainer = styled(Card)(
+  ({ theme: { up, breakpoints } }) => css`
+    max-width: 975px;
+    margin: 0 auto;
+    margin-top: 59px;
+
+    ${up(breakpoints.sm)} {
+      margin-top: 100px;
+    }
+  `
+);
+
+export const EditForm = styled.form`
+  padding: 20px;
+`;
+
+export const Image = styled.div(
+  ({ theme: { up } }) => css`
+    ${flex.alignCenter}
+    width: 100%;
+    height: 60px;
+    padding: 15px;
+
+    img {
+      width: 38px;
+      height: 38px;
+      margin-right: 20px;
+    }
+
+    h1 {
+      color: #262626;
+      font-size: 20px;
+      line-height: 22px;
+      margin-top: -10px;
+      margin-bottom: 2px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    label {
+      color: #0095f6;
+      font-size: 14px;
+      font-weight: 600;
+      cursor: pointer;
+    }
+
+    ${up(735)} {
+      img {
+        margin: 2px 32px 0 65px;
+      }
+    }
+  `
+);
+
+export const PropLabel = styled.label(
+  ({ theme: { up } }) => css`
+    color: #262626;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 18px;
+    margin-top: 6px;
+    flex-basis: 25px;
+    padding: 0 20px;
+
+    ${up(735)} {
+      flex: 0 0 150px;
+      padding-left: 32px;
+      padding-right: 32px;
+      text-align: right;
+    }
+  `
+);
+
+export const InputDesc = styled.div`
+  width: 100%;
+  margin-bottom: 25px;
+
+  p {
+    font-size: 12px;
+    line-height: 16px;
+    margin: -2px 0 -3px;
+    color: #8e8e8e;
+    font-weight: 400;
+    margin-top: 10px;
+  }
+`;
