ca7e486a9a25ff2b83b29c0340984a2a47a4042e
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-02-08 01:08:56 +0200

    fix add-comment component

---
+++ src/components/common/add-comment/index.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { setSelectedPostCommentsThunk } from "store/thunks/post";
+
+import Textarea from "components/common/textarea";
+
+import { api } from "api";
+
+import { TextareaContainer } from "./styles";
+
+const AddComment = ({ postId }) => {
+  const dispatch = useDispatch();
+
+  const [commentText, setCommentText] = useState("");
+
+  const { id: userId } = useSelector((state) => state.user.currentUser);
+
+  const onChange = (e) => {
+    setCommentText(e.target.value);
+  };
+
+  const onClick = async () => {
+    await api.post.setUserComment(postId, userId, commentText);
+    dispatch(setSelectedPostCommentsThunk(postId));
+
+    setCommentText("");
+  };
+
+  return (
+    <TextareaContainer>
+      <Textarea
+        id="comment"
+        type="comment"
+        size="full"
+        border="none"
+        bgColor="transparent"
+        placeholder="Add a comment..."
+        value={commentText}
+        onChange={onChange}
+        onClick={() => onClick(postId, userId, commentText)}
+      />
+    </TextareaContainer>
+  );
+};
+
+export default AddComment;
