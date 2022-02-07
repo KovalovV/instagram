ee8fdd435e152465fc4c58012d2842f154b686ee
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-02-08 01:05:58 +0200

    home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,147 @@
-import { FormContainer } from "./styles";
+/* eslint-disable no-unused-vars */
+import { useState, useEffect, useCallback } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-const Home = () => <FormContainer>home</FormContainer>;
+import { api } from "api";
+
+import ShortUserInfo from "components/common/short-user-info";
+import PostActions from "components/common/post-actions";
+import UserComment from "components/common/user-comment";
+import AddComment from "components/common/add-comment";
+import UserSuggestions from "components/home/user-suggestions";
+
+import LoaderScreen from "components/common/loader-screen";
+
+import { Icon } from "components/common/icons";
+import Date from "components/common/date";
+
+import {
+  MainContainer,
+  UserFeed,
+  Post,
+  Image,
+  LikeInfo,
+  AsideContainer,
+  AsideFeed,
+  HeaderDetails,
+  HelpList,
+  AsideFooter,
+} from "./styles";
+
+const Home = () => {
+  const [feed, setFeed] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id, avatar, name, login, saved } = useSelector(
+    (state) => state.user.currentUser
+  );
+
+  const helpUserCategory = [
+    "About",
+    "Help",
+    "Press",
+    "API",
+    "Jobs",
+    "Privacy",
+    "Terms",
+    "Locations",
+    "Top",
+    "Accounts",
+    "Hashtags",
+    "Language",
+  ];
+
+  const onChangeFeed = (postId, isLiked) => {
+    setFeed((prevState) => {
+      const indexChangedPost = prevState.findIndex(
+        (post) => post.id === postId
+      );
+      const leftPart = prevState.slice(0, indexChangedPost);
+      const rightPart = prevState.slice(indexChangedPost + 1);
+
+      const changedPost = {
+        ...prevState[indexChangedPost],
+        likes: isLiked
+          ? prevState[indexChangedPost].likes.filter((userId) => userId !== id)
+          : [...prevState[indexChangedPost].likes, id],
+      };
+
+      return [...leftPart, changedPost, ...rightPart];
+    });
+  };
+
+  const fetchPostData = useCallback(async () => {
+    setIsLoading(true);
+    const posts = await api.user.getUserFeed(id);
+    setFeed(posts);
+    setIsLoading(false);
+  }, [id]);
+
+  useEffect(() => {
+    const componentMounted = new AbortController();
+
+    fetchPostData();
+
+    return () => componentMounted.abort();
+  }, [fetchPostData]);
+
+  if (isLoading) {
+    return <LoaderScreen />;
+  }
+
+  return (
+    <MainContainer>
+      <UserFeed>
+        {feed &&
+          feed.map((post, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <Post key={`${post.id}-${index}`}>
+              <ShortUserInfo userId={post.userID} />
+              <Image>
+                <img src={post.image} alt="Post" />
+              </Image>
+              <PostActions
+                currentUserId={id}
+                saved={saved}
+                postId={post.id}
+                likes={post.likes}
+                onChangeFeed={onChangeFeed}
+              />
+              <LikeInfo>{post.likes && post.likes.length} likes</LikeInfo>
+              <UserComment
+                userId={post.userID}
+                description={post.description}
+              />
+              <Date date={post.timestamp} marginLeft />
+              <AddComment postId={post.id} />
+            </Post>
+          ))}
+      </UserFeed>
+      <AsideContainer>
+        <AsideFeed>
+          <HeaderDetails>
+            <Link to={`/${login}`}>
+              <img src={avatar} alt="Avatar" />
+            </Link>
+            <div>
+              <h1>
+                <Link to={`/${login}`}>{login}</Link>
+              </h1>
+              <p>{name}</p>
+            </div>
+          </HeaderDetails>
+          <UserSuggestions />
+          <HelpList>
+            {helpUserCategory.map((category) => (
+              <li key={category}>{category}</li>
+            ))}
+          </HelpList>
+          <AsideFooter>© 2022 Instagram from Vitala Kovalov</AsideFooter>
+        </AsideFeed>
+      </AsideContainer>
+    </MainContainer>
+  );
+};
 
 export default Home;
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,5 +1,151 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { flex } from "utils/flex";
 
-export const FormContainer = styled.div`
-  margin-top: 100px;
+export const MainContainer = styled.div(
+  ({ theme: { up, breakpoints } }) => css`
+    display: flex;
+    position: relative;
+
+    margin: 0 auto;
+    margin-top: 60px;
+    max-width: 935px;
+
+    ${up(breakpoints.sm)} {
+      margin-top: 90px;
+    }
+  `
+);
+
+export const UserFeed = styled.div(
+  ({ theme: { up, breakpoints } }) => css`
+    max-width: 935px;
+    width: 100%;
+
+    ${up(breakpoints.lg)} {
+      max-width: 614px;
+      margin-right: 28px;
+    }
+  `
+);
+
+export const LikeInfo = styled.div`
+  padding: 0px 16px;
+  color: #262626;
+  font-weight: 600;
+`;
+
+export const Post = styled.div(
+  ({ theme: { up, breakpoints } }) => css`
+    max-width: 614px;
+    background-color: #fff;
+    border: 1px solid #dbdbdb;
+    margin: 0 auto;
+    margin-bottom: 24px;
+
+    ${up(breakpoints.lg)} {
+    }
+  `
+);
+
+export const Image = styled.div`
+  width: 100%;
+  height: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const AsideContainer = styled.div`
+  position: fixed;
+  margin: 0 auto;
+  width: 935px;
+`;
+
+export const AsideFeed = styled.aside(
+  ({ theme: { up, breakpoints } }) => css`
+    display: none;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    width: 100%;
+    max-width: 293px;
+    height: 100vh;
+    margin-bottom: 30px;
+
+    ${up(breakpoints.lg)} {
+      display: block;
+    }
+  `
+);
+
+export const AsideFooter = styled.footer`
+  max-width: 100%;
+  text-transform: uppercase;
+  color: #c7c7c7;
+  font-size: 11px;
+  font-weight: 400;
+  line-height: 13px;
+`;
+
+export const HeaderDetails = styled.div`
+  ${flex.alignCenter}
+  width: 100%;
+  height: 60px;
+  margin-bottom: 10px;
+  margin-top: 18px;
+
+  img {
+    width: 56px;
+    height: 56px;
+    object-fit: cover;
+    border-radius: 50%;
+    margin-right: 14px;
+  }
+
+  h1 {
+    color: #262626;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    margin-top: -5px;
+  }
+
+  p {
+    color: #8e8e8e;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+  }
+`;
+
+export const HelpList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0px 10px;
+  margin-bottom: 16px;
+
+  li {
+    ${flex.alignCenter}
+    position: relative;
+    color: #c7c7c7;
+    font-size: 11px;
+    font-weight: 400;
+    line-height: 13px;
+    text-transform: none;
+  }
+
+  li:after {
+    content: "";
+    position: absolute;
+    display: block;
+    right: -9px;
+    transform: translateX(-50%);
+    scale: 0.5;
+    background-color: #c7c7c7;
+    height: 4px;
+    width: 4px;
+    border-radius: 50%;
+  }
 `;
