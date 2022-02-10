c5806df54b40d78a29d7f7f15644345d3e9e41a0
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-02-10 23:51:10 +0200

    add skeleton

--- src/components/common/short-user-info/index.jsx
+++ src/components/common/short-user-info/index.jsx
@@ -1,33 +1,58 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Skeleton from "@mui/material/Skeleton";
 
 import { api } from "api";
 
-import { Link } from "react-router-dom";
-
 import { HeaderDetails } from "./styles";
 
-const ShortUserInfo = ({ userId }) => {
+const ShortUserInfo = ({ userId, width, height, withName }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const userRes = await api.user.getUserById(userId);
       setUser(userRes && userRes.data());
+      setIsLoading(false);
     };
     fetchData();
   }, [userId]);
 
   return (
-    <HeaderDetails>
-      {user && (
-        <>
-          <Link to={`/u/${user.login}`}>
-            <img src={user.avatar} alt="Avatar" />
-          </Link>
-          <h1>
-            <Link to={`/u/${user.login}`}>{user.login}</Link>
-          </h1>
-        </>
+    <HeaderDetails width={width} height={height}>
+      {isLoading ? (
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Box sx={{ margin: 1 }}>
+            <Skeleton variant="circular">
+              <Avatar />
+            </Skeleton>
+          </Box>
+          <Box sx={{ width: "100%" }}>
+            <Skeleton width="125px">
+              <Typography>.</Typography>
+            </Skeleton>
+          </Box>
+        </Box>
+      ) : (
+        user && (
+          <>
+            <Link to={`/u/${user.login}`}>
+              <img src={user.avatar} alt="Avatar" />
+            </Link>
+            <div>
+              <h1>
+                <Link to={`/u/${user.login}`}>{user.login}</Link>
+              </h1>
+              {withName && <p>{user.name}</p>}
+            </div>
+          </>
+        )
       )}
     </HeaderDetails>
   );
--- src/components/common/short-user-info/styles.js
+++ src/components/common/short-user-info/styles.js
@@ -1,24 +1,34 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { flex } from "utils/flex";
 
-export const HeaderDetails = styled.div`
-  ${flex.alignCenter}
-  width: 100%;
-  height: 60px;
-  padding: 15px;
+export const HeaderDetails = styled.div(
+  ({ width, height }) => css`
+    ${flex.alignCenter}
+    width: 100%;
+    height: 60px;
+    padding: 15px;
 
-  img {
-    width: 32px;
-    height: 32px;
-    object-fit: cover;
-    border-radius: 50%;
-    margin-right: 14px;
-  }
+    img {
+      width: ${width};
+      height: ${height};
+      object-fit: cover;
+      border-radius: 50%;
+      margin-right: 14px;
+    }
 
-  h1 {
-    color: #262626;
-    font-weight: 600;
-    font-size: 16px;
-    line-height: 24px;
-  }
-`;
+    h1 {
+      color: #262626;
+      font-weight: 600;
+      font-size: 16px;
+      line-height: 24px;
+      margin-top: -5px;
+    }
+
+    p {
+      color: #8e8e8e;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 18px;
+    }
+  `
+);
